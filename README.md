52c5104b62b87785fb85dbc9f93bc366d62a6376
Eric Moynihan <e.moynihan00@gmail.com>
2018-08-01 19:41:32 +0100

    added weeks from date

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 > Angular 4 app
 
 > type 'npm install' to install dependencies
+
+> node-v: v8.11.1
+> npm -v: 5.6.0
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,17 +13,51 @@ export class DashboardComponent {
   endDate = moment().endOf('week').add(0,'weeks').format('MMMM Do YYYY');
 
   weekTemplate = "This week";
+
   goNext() {
     this.add++;
     this.startDate = moment().startOf('week').add(this.add,'weeks').format('MMMM Do YYYY');
     this.endDate = moment().endOf('week').add(this.add,'weeks').format('MMMM Do YYYY');
-    console.log(this.add);
+    if (this.add == 0) {
+      this.weekTemplate = "This week"
+    } else {
+      if (this.add == 1 || this.add == -1) {
+        if (this.add < 0) {
+          this.weekTemplate = this.add*-1 + " week from now"
+        } else {
+          this.weekTemplate = this.add + " week from now"
+        }
+      } else {
+        if (this.add < 0) {
+          this.weekTemplate = this.add*-1 + " weeks from now"
+        } else {
+          this.weekTemplate = this.add + " weeks from now"
+        }
+      }
+
+    }
   }
   goLast() {
     this.add--;
     this.startDate = moment().startOf('week').add(this.add,'weeks').format('MMMM Do YYYY');
     this.endDate = moment().endOf('week').add(this.add,'weeks').format('MMMM Do YYYY');
-    console.log(this.add);
+    if (this.add == 0) {
+      this.weekTemplate = "This week"
+    } else {
+      if (this.add == 1 || this.add == -1) {
+        if (this.add < 0) {
+          this.weekTemplate = this.add*-1 + " week ago"
+        } else {
+          this.weekTemplate = this.add + " week ago"
+        }
+      } else {
+        if (this.add < 0) {
+          this.weekTemplate = this.add*-1 + " weeks ago"
+        } else {
+          this.weekTemplate = this.add + " weeks ago"
+        }
+      }
+    }
   }
 
 
