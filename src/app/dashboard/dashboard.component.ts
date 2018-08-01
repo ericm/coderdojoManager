27a0a16ab0e706f2bcb43da8611e49385c2e67db
Eric Moynihan <e.moynihan00@gmail.com>
2018-08-02 00:33:27 +0100

    fixed weeks from now

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,43 @@
 import {Component} from '@angular/core';
 import * as moment from 'moment';
 
+import * as Nano  from 'nano'
+/*
+let db = Nano('http://127.0.0.1:5984/calendar').db.;
+
+interface Entry extends Nano.MaybeDocument {
+  date: string,
+  entry: string
+}
+
+class Person implements Entry {
+  _id: string;
+  _rev: string;
+  date: string;
+  entry: string;
+
+  constructor(date: string, entry: string) {
+    this._id = undefined;
+    this._rev = undefined;
+    this.date = date;
+    this.entry = entry
+  }
+
+  processAPIResponse(response: Nano.DocumentInsertResponse) {
+    if (response.ok === true) {
+      this._id = response.id;
+      this._rev = response.rev
+    }
+  }
+}
+let enter = new Person('02/08/2018', 'meeting');
+db.use("calendar").insert(enter).then((response) => {
+  enter.processAPIResponse(response);
+  console.log(enter);
+});
+*/
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -15,6 +52,7 @@ export class DashboardComponent {
   weekTemplate = "This week";
 
   goNext() {
+
     this.add++;
     this.startDate = moment().startOf('week').add(this.add,'weeks').format('MMMM Do YYYY');
     this.endDate = moment().endOf('week').add(this.add,'weeks').format('MMMM Do YYYY');
@@ -23,13 +61,13 @@ export class DashboardComponent {
     } else {
       if (this.add == 1 || this.add == -1) {
         if (this.add < 0) {
-          this.weekTemplate = this.add*-1 + " week from now"
+          this.weekTemplate = this.add*-1 + " week ago"
         } else {
           this.weekTemplate = this.add + " week from now"
         }
       } else {
         if (this.add < 0) {
-          this.weekTemplate = this.add*-1 + " weeks from now"
+          this.weekTemplate = this.add*-1 + " weeks ago"
         } else {
           this.weekTemplate = this.add + " weeks from now"
         }
@@ -48,13 +86,13 @@ export class DashboardComponent {
         if (this.add < 0) {
           this.weekTemplate = this.add*-1 + " week ago"
         } else {
-          this.weekTemplate = this.add + " week ago"
+          this.weekTemplate = this.add + " week from now"
         }
       } else {
         if (this.add < 0) {
           this.weekTemplate = this.add*-1 + " weeks ago"
         } else {
-          this.weekTemplate = this.add + " weeks ago"
+          this.weekTemplate = this.add + " weeks from now"
         }
       }
     }
