000b2487b2bf186613f644e452be0589dbf6f47b
Eric Moynihan <e.moynihan00@gmail.com>
2018-08-09 19:50:53 +0100

    added list base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import {Component} from '@angular/core';
+/*import * as Cloudant from '@cloudant/cloudant';
 
+let client = Cloudant("https://1c7e8b98-ff80-417a-bc1e-75913b780bae-bluemix:f7bf9f63e1860dc8940fcf7d1d143122fdffe5b6e70c16534e96ce9c2dda141b@1c7e8b98-ff80-417a-bc1e-75913b780bae-bluemix.cloudant.com");
+
+(client.db.list() as any).then((d) => { console.log(d); });
+*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  loggedIn = false;
+  loggedIn = true;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  CLOUDANT_URL: "https://1c7e8b98-ff80-417a-bc1e-75913b780bae-bluemix:f7bf9f63e1860dc8940fcf7d1d143122fdffe5b6e70c16534e96ce9c2dda141b@1c7e8b98-ff80-417a-bc1e-75913b780bae-bluemix.cloudant.com"
 };
 
 /*
